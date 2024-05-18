448609716b8d859e38ab5ad371e61075f09d875c
nKadykov <nkadykov@gmail.com>
2024-05-18 21:54:34 +0300

    init

---
+++ window.cpp
@@ -0,0 +1,203 @@
+#include "window.h"
+#include <QtMath>
+
+Window::Window(QWidget* parent) : QWidget(parent), m_timer(new QTimer(this)), m_elapsed_time(new QElapsedTimer()) {
+    m_table = new QTableWidget(this);
+    m_table->setRowCount(20);
+    m_table->setColumnCount(4);
+    m_window_widget = new QWidget();
+    QTableWidgetItem* item1 = new QTableWidgetItem;
+    item1->setText(tr("Mode"));
+    m_table->setItem(0, 0, item1);
+    QTableWidgetItem* item2 = new QTableWidgetItem;
+    item2->setText(tr("Time"));
+    m_table->setItem(0, 1, item2);
+    QTableWidgetItem* item3 = new QTableWidgetItem;
+    item3->setText(tr("Fitts"));
+    m_table->setItem(0, 2, item3);
+    QTableWidgetItem* item4 = new QTableWidgetItem;
+    item4->setText(tr("Length"));
+    m_table->setItem(0, 3, item4);
+
+    for(int i = 0; i < NUMBER_MODE; ++i) {
+        m_buttons_mode[i] = createButton(tr("MODE: ") + QString::number(i + 1), &Window::setMode);
+        m_buttons_mode[i]->setStyleSheet("background-color: white");
+    }
+
+    connect(m_timer, &QTimer::timeout, this, &Window::timeHit);
+
+    m_buttons_layout = new QVBoxLayout;
+    for(int i = 0; i < NUMBER_MODE; ++i) {
+        m_buttons_layout->addWidget(m_buttons_mode[i]);
+    }
+    m_buttons_box = new QGroupBox(tr("Mode"));
+    m_buttons_box->setLayout(m_buttons_layout);
+
+    m_window_layout = new QGridLayout;
+    m_window_box = new QGroupBox;
+    m_window_box->setLayout(m_window_layout);
+    m_window_box->setFixedSize(QSize(1280, 720));
+
+    m_table_box = new QGroupBox(tr("Table"));
+    m_table_layout = new QGridLayout;
+    m_table_layout->addWidget(m_table);
+    m_table_box->setLayout(m_table_layout);
+    m_table_box->setFixedSize(QSize(400, 500));
+
+    m_main_layout = new QGridLayout;
+    m_main_layout->addWidget(m_window_box, 0, 0, 2, 1);
+    m_main_layout->addWidget(m_buttons_box, 0, 1, 1, 1);
+    m_main_layout->addWidget(m_table_box, 1, 1, 1, 1);
+    setLayout(m_main_layout);
+
+    m_timer->setInterval(1000);
+    m_elapsed_time->start();
+    m_timer->start();
+}
+
+template<typename PointerToMemberFunction>
+Button* Window::createButton(const QString &text, const PointerToMemberFunction &member) {
+    Button *button = new Button(text);
+    connect(button, &Button::clicked, this, member);
+    return button;
+}
+
+template<typename PointerToMemberFunction>
+Button* Window::createNewButton(const QString &text, const PointerToMemberFunction &member) {
+    Button *button = new Button(text, m_window_widget);
+    connect(button, &Button::clicked, this, member);
+    return button;
+}
+
+void Window::clickButton() {
+    Button *clicked_button = qobject_cast<Button*>(sender());
+    m_is_created = false;
+    int e = m_elapsed_time->elapsed();
+
+    if(index > 20) {
+        index = 1;
+    }
+
+    QTableWidgetItem* item1 = new QTableWidgetItem;
+    item1->setText(QString::number(static_cast<int>(m_mode) + 1));
+    m_table->setItem(index, 0, item1);
+    QTableWidgetItem* item2 = new QTableWidgetItem;
+    item2->setText(QString::number(e / 1000) + "." + QString::number(e % 1000));
+    m_table->setItem(index, 1, item2);
+    QTableWidgetItem* item3 = new QTableWidgetItem;
+    item3->setText(QString::number(this->countFitts()));
+    m_table->setItem(index, 2, item3);
+    QTableWidgetItem* item4 = new QTableWidgetItem;
+    item4->setText(QString::number(this->getLength()));
+    m_table->setItem(index, 3, item4);
+
+    m_table->setItem(index, 2, item3);
+    m_elapsed_time->restart();
+    index++;
+    m_window_layout->removeWidget(m_window_widget);
+    m_window_widget = new QWidget();
+    m_button_x = 0;
+    clicked_button->deleteLater();
+}
+
+double Window::countFitts() {
+    return qLn(this->getLength() / 100 + 1) / qLn(2);
+}
+
+int Window::getLength() {
+    int length = qSqrt(qPow(m_button_x + 50, 2) + qPow(m_button_y + 50, 2));
+    return length;
+}
+
+void Window::setMode() {
+    Button *clicked_button = qobject_cast<Button*>(sender());
+    if(clicked_button == m_buttons_mode[0]) {
+        m_mode = Mode::FIRST;
+        m_table->clear();
+        QTableWidgetItem* item1 = new QTableWidgetItem;
+        item1->setText(tr("Mode"));
+        m_table->setItem(0, 0, item1);
+        QTableWidgetItem* item2 = new QTableWidgetItem;
+        item2->setText(tr("Time"));
+        m_table->setItem(0, 1, item2);
+        QTableWidgetItem* item3 = new QTableWidgetItem;
+        item3->setText(tr("Fitts"));
+        m_table->setItem(0, 2, item3);
+        QTableWidgetItem* item4 = new QTableWidgetItem;
+        item4->setText(tr("Length"));
+        m_table->setItem(0, 3, item4);
+        index = 1;
+    }
+    if(clicked_button == m_buttons_mode[1]) {
+        m_mode = Mode::SECOND;
+        m_table->clear();
+        QTableWidgetItem* item1 = new QTableWidgetItem;
+        item1->setText(tr("Mode"));
+        m_table->setItem(0, 0, item1);
+        QTableWidgetItem* item2 = new QTableWidgetItem;
+        item2->setText(tr("Time"));
+        m_table->setItem(0, 1, item2);
+        QTableWidgetItem* item3 = new QTableWidgetItem;
+        item3->setText(tr("Fitts"));
+        m_table->setItem(0, 2, item3);
+        QTableWidgetItem* item4 = new QTableWidgetItem;
+        item4->setText(tr("Length"));
+        m_table->setItem(0, 3, item4);
+        index = 1;
+    }
+    if(clicked_button == m_buttons_mode[2]) {
+        m_mode = Mode::THIRD;
+        m_table->clear();
+        QTableWidgetItem* item1 = new QTableWidgetItem;
+        item1->setText(tr("Mode"));
+        m_table->setItem(0, 0, item1);
+        QTableWidgetItem* item2 = new QTableWidgetItem;
+        item2->setText(tr("Time"));
+        m_table->setItem(0, 1, item2);
+        QTableWidgetItem* item3 = new QTableWidgetItem;
+        item3->setText(tr("Fitts"));
+        m_table->setItem(0, 2, item3);
+        QTableWidgetItem* item4 = new QTableWidgetItem;
+        item4->setText(tr("Length"));
+        m_table->setItem(0, 3, item4);
+        index = 1;
+    }
+}
+
+void Window::timeHit() {
+    int time = 500;
+    int elapsed = m_elapsed_time->elapsed();
+    int index;
+    int keyboard_part = 0;
+    if(elapsed > time && (!m_is_created)) {
+        switch (m_mode) {
+            case Mode::FIRST:
+                m_button_x = 1150;
+                m_button_y = 0;
+                new_button = createNewButton(tr(""), &Window::clickButton);
+                new_button->setGeometry(m_button_x, m_button_y, 100, 100);
+                m_window_layout->addWidget(m_window_widget);
+                m_is_created = true;
+                m_elapsed_time->start();
+                break;
+            case Mode::SECOND:
+                m_button_x = rand() % 1150;
+                m_button_y = 0;
+                new_button = createNewButton(tr(""), &Window::clickButton);
+                new_button->setGeometry(m_button_x, m_button_y, 100, 100);
+                m_window_layout->addWidget(m_window_widget);
+                m_is_created = true;
+                m_elapsed_time->start();
+                break;
+            case Mode::THIRD:
+                m_button_x = rand() % 1150;
+                m_button_y = rand() % 500;
+                new_button = createNewButton(tr(""), &Window::clickButton);
+                new_button->setGeometry(m_button_x, m_button_y, 100, 100);
+                m_window_layout->addWidget(m_window_widget);
+                m_is_created = true;
+                m_elapsed_time->start();
+                break;
+        }
+    }
+}
---
+++ window.h
@@ -0,0 +1,68 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include "button.h"
+
+#include <QWidget>
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QFormLayout>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QScrollArea>
+
+QT_BEGIN_NAMESPACE
+class QLineEdit;
+QT_END_NAMESPACE
+class Button;
+
+class Window : public QWidget
+{
+    Q_OBJECT
+public:
+    Window(QWidget* parent = nullptr);
+    void setMode();
+    double countFitts();
+    int getLength();
+private slots:
+    void clickButton();
+    void timeHit();
+private:
+    template<typename PointerToMemberFunction>
+    Button* createButton(const QString&, const PointerToMemberFunction&);
+    template<typename PointerToMemberFunction>
+    Button* createNewButton(const QString&, const PointerToMemberFunction&);
+    Button* button;
+    enum class Mode {
+        FIRST = 0,
+        SECOND,
+        THIRD
+    };
+    bool m_is_created = false;
+    QTimer* m_timer;
+    QElapsedTimer* m_elapsed_time;
+    Mode m_mode = Mode::FIRST;
+    QTableWidget *m_table;
+    enum {NUMBER_MODE = 3};
+    Button* m_buttons_mode[NUMBER_MODE];
+    int m_time = 0;
+    int index = 1;
+
+    QScrollArea* scroll_area;
+    QGroupBox* m_table_box;
+    QGridLayout* m_table_layout;
+    QGroupBox *m_buttons_box;
+    QWidget* m_window_widget;
+    Button* new_button;
+    QGridLayout* m_window_layout;
+    QGroupBox *m_window_box;
+    QWidget* m_buttons_widget;
+    QVBoxLayout* m_buttons_layout;
+    QGridLayout* m_main_layout;
+
+    int m_button_x = 0;
+    int m_button_y = 0;
+};
+
+#endif // WINDOW_H
